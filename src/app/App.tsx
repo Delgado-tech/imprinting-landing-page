9713010686bd2293acc1cce76979f764dec06837
Joao Pedro <cls>
2024-03-14 12:51:20 -0300

    add/feat:Cards

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,8 @@ import ImprintingCoin from "../components/home/ImprintingCoin.tsx";
 import Introduction from "../components/home/Introduction.tsx";
 import NeuroMaps from "../components/home/NeuroMaps.tsx";
 import ProgramIAM from "../components/home/ProgramIAM.tsx";
-import NavBar from "../components/home/NavBar.tsx"
+import NavBar from "../components/home/NavBar.tsx";
+import Cards from "../components/home/Cards.tsx"
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<main>
 				<NavBar />
 				<Introduction />
+				<Cards />
 				<NeuroMaps />
 				<ProgramIAM />
 				<ImprintingCoin />
