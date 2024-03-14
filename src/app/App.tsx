863a848a255c6442e05290a1c99b14f15ed0cc0c
Joao Pedro <cls>
2024-03-14 12:40:53 -0300

    add/feat:NavBAr

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,14 @@ import ImprintingCoin from "../components/home/ImprintingCoin.tsx";
 import Introduction from "../components/home/Introduction.tsx";
 import NeuroMaps from "../components/home/NeuroMaps.tsx";
 import ProgramIAM from "../components/home/ProgramIAM.tsx";
+import NavBar from "../components/home/NavBar.tsx"
 
 function App() {
 	return (
 		<section className="h-screen">
 			{/* <Logo color="red" /> */}
 			<main>
+				<NavBar />
 				<Introduction />
 				<NeuroMaps />
 				<ProgramIAM />
