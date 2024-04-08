62aa2bbefb8af216bba857217a661202113626fd
Felipe Destro <felipe.s.destro@gmail.com>
2024-04-08 06:36:16 -0400

    edit/feat: controller localstorage

--- src/controllers/LocalStorageController.ts
+++ src/controllers/LocalStorageController.ts
@@ -13,11 +13,16 @@ class LocalStorageControllers {
 	static checkTime(): boolean {
 		const local = localStorage;
 
-		if (local.getItem("time")) {
-			const time_local = Number(local.getItem("date"));
+		if (local.getItem("time") && local.getItem("turn")) {
+			const time_local = Number(local.getItem("time"));
+			const turn_local = Number(local.getItem("turn"));
+
 			const time_now = new Date().getTime();
 
-			if (time_now - time_local < 1) {
+			if (turn_local < 3) {
+				local.setItem("turn", JSON.stringify(turn_local + 1));
+				return true;
+			} else if (time_now - time_local < 1) {
 				return true;
 			} else {
 				return false;
@@ -25,7 +30,7 @@ class LocalStorageControllers {
 		} else {
 			const time = new Date().getTime();
 			local.setItem("time", JSON.stringify(time));
-
+			local.setItem("turn", JSON.stringify(1));
 			return true;
 		}
 	}
